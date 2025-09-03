af2d611eee9690bde68f8ea08611ae72a5191b64
yanamadalaraju <150237963+yanamadalaraju@users.noreply.github.com>
2025-09-03 12:06:06 +0530

    changes

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 // src/config.ts
-export const BASE_URL: string = 'http://69.62.81.122:8080';
-
-// export const BASE_URL: string = 'http://localhost:5000';
+// export const BASE_URL: string = 'http://69.62.81.122:8080';
+// export const BASE_URL: string = 'https://nowalnaturecare.com:5000';
+// export const BASE_URL: string = '/api';
+// export const BASE_URL = '/api';
+// const baseURL = '/api';
+export const BASE_URL: string = 'http://localhost:5000';
